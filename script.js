d2478ab63b2b58af55167ca1010bde8b5f300fe2
Pravesh532 <praveshpatel9569@gmail.com>
2025-03-18 16:24:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function volume_sphere() {
     //Write your code here
+	let radi = document.getElementById('radius').value;
+	 let volumeOutput = document.getElementBuId("volume");
+	let radius = parseFloat(radi);
+	 if (isNaN(radius) || radius < 0) {
+        volumeOutput.value = "NaN";
+        return;
+    }
+	 let volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+	volumeOutput.value = volume.toFixed(4);
+	
   
 } 
 
